be9fd3420b3bca0529c16aae591f5b61779adec5
Shikhar <krtonia@gmail.com>
2024-06-30 15:50:40 +0530

    beryllium: Add some flags and drop dependencies

--- lineage_beryllium.mk
+++ lineage_beryllium.mk
@@ -9,6 +9,12 @@ $(call inherit-product, device/xiaomi/beryllium/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Bootanimation Resolution
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Blur
+TARGET_ENABLE_BLUR := false
+
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_beryllium
 PRODUCT_DEVICE := beryllium
