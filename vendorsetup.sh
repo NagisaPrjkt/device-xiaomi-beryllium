9db80a19e23b14a3222f9bf37f5fba8c2e85ea17
Shikhar <krtonia@gmail.com>
2024-06-30 15:43:06 +0530

    beryllium: Add vendorsetup

---
+++ vendorsetup.sh
@@ -0,0 +1,8 @@
+# common tree
+git clone https://github.com/Krtonia/device_xiaomi_sdm845-common.git -b 14 device/xiaomi/sdm845-common
+
+# Radioactive kernel
+git clone https://github.com/Krtonia/Radioactive_kernel.git -b 14 --depth=1 kernel/xiaomi/sdm845
+
+# vendor 
+git clone https://github.com/Krtonia/beryllium-vendor.git -b 14 vendor/xiaomi
